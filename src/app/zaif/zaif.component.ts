2f49c19a19538cea485b2cda7221f27c6c7b9135
hosen <nevele49@yahoo.co.jp>
2018-04-21 02:40:28 +0900

    zaif

--- src/app/zaif/zaif.component.ts
+++ src/app/zaif/zaif.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { WebsocketService } from '../services/websocket.service';
+import { ZaifService } from '../services/zaif.service';
 
 @Component({
-  selector: 'app-zaif',
+  selector: 'zaif',
   templateUrl: './zaif.component.html',
   styleUrls: ['./zaif.component.css']
 })
 export class ZaifComponent implements OnInit {
 
-  constructor() { }
+  bids: number[];
+  asks: number[];
+  ii;
+  constructor(private zaif:ZaifService) {
+    this.ii = zaif.messages.subscribe(msg => {
+      this.bids = msg.bids;
+      this.asks = msg.asks;
+      // console.log('Response: ' + msg.bids);
+      // console.log('Response: ' + msg.asks);
+    });
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(){
+    this.ii.unsubscribe();
+  }
 }
